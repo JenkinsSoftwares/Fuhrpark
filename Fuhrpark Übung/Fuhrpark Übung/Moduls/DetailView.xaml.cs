4e3e02c31730af7107c69d079701aa01afa25abf
Lukas Bube <lukas.bube@tecalliance.net>
2018-01-09 08:22:00 +0100

    added views

---
+++ Fuhrpark Übung/Fuhrpark Übung/Moduls/DetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MusicApp.Moduls.Detail
+{
+    /// <summary>
+    /// Interaction logic for DetailView.xaml
+    /// </summary>
+    public partial class DetailView : UserControl
+    {
+        public DetailView( )
+        {
+            InitializeComponent( );
+        }
+    }
+}
---
+++ Fuhrpark Übung/Fuhrpark Übung/Moduls/SelectionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MusicApp.Moduls.Selection
+{
+    /// <summary>
+    /// Interaction logic for SelectionView.xaml
+    /// </summary>
+    public partial class SelectionView : UserControl
+    {
+        public SelectionView( )
+        {
+            InitializeComponent( );
+        }
+    }
+}
